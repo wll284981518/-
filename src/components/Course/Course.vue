61b8bcb0e6777134dc89139477e4eafd94744424
wll284981518 <284981518@qq.com>
2019-12-20 09:44:10 +0800

    wll

--- src/components/Course/Course.vue
+++ src/components/Course/Course.vue
@@ -1,15 +1,217 @@
 <template>
-    <div>
-      课程
-    </div>
+        <div class="wll_Course">
+            <div class="wll_top">
+                <div class="wll_tsk">
+                    <h2>特色课</h2>
+                    <img src="../../../static/course-img/wll_ glass.jpg" alt="">
+                </div>
+                <div class="wll_kj">
+                    <van-dropdown-menu>
+                        <van-dropdown-item title="分类" v-model="wll_value1" :options="wll_option1" />
+                        <van-dropdown-item title="排序" v-model="wll_value2" :options="wll_option2" />
+                        <van-dropdown-item title="筛选" v-model="wll_value3" :options="wll_option3" />
+                    </van-dropdown-menu>
+                </div>
+            </div>
+            <div class="wll_conter">
+               <ul class="wll_ul">
+                   <li v-for="(item,key) in wll_course" :key="key" class="wll_li"> 
+                       <router-link :to="'/details?id='+item.id" tag="div" class="wll_div">
+                            <div>
+                                <h2>{{item.name}}</h2>
+                            </div>
+                            <div>
+                                <span>{{item.time}}</span>
+                                <span>{{item.lesson}}</span>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <img class="wll_img1" :src="'../../../static/course-img/'+item.img" alt="">
+                                        <span>{{item.teachername}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <p>{{item.count}}</p>
+                                <p>{{item.price}}</p>
+                            </div>
+                     </router-link>
+                   </li>
+               </ul>
+            </div>
+        </div>
 </template>
 
 <script>
+    //引入axios请求器
+    import axios  from "axios";
     export default {
-        name: "Course"
+        name: "Course",
+        data() {
+            return {
+                wll_value1: 0,
+                wll_value2: 'a',
+                wll_value3: 'I',
+                wll_option1: [
+                    { text: '年级', value: 0 },
+                    { text: '学科', value: 1 },
+                ],
+                wll_option2: [
+                    { text: '综合排序', value: 'a' },
+                    { text: '最新', value: 'b' },
+                    { text: '最热', value: 'c' },
+                    { text: '价格从低到高', value: 'd' },
+                    { text: '价格从高到低', value: 'e' },
+                ],
+                wll_option3: [
+                    { text: '默认排序', value: 'I' },
+                    { text: '好评排序', value: 'II' },
+                    { text: '销量排序', value: 'IIi' },
+                ],
+                collect:JSON.parse(localStorage.getItem("collect")) || [],
+                wll_course:[]
+            }
+        },
+        mounted() {
+            axios.get("../../../static/date.json").then((msg)=>{
+                console.log(msg);
+                this.wll_course=msg.data.data;
+            })
+        },
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+    body,html,.wll_Course{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction:column;
+        .wll_top{
+            width: 100%;
+            height: 1.44rem;
+            .wll_tsk{
+                width: 100%;
+                height: 0.74rem;
+                display: flex;
+                align-items: center;
+                h2{
+                    margin-left: 2.72rem;
+                    color: #4d4d4d;
+                    font-size: 0.26rem;
+                    font-weight: normal;
+                }
+                img{
+                    width: 0.36rem;
+                    height: 0.36rem;
+                    margin-left: 2.22rem;
+                }
+            }
+            .wll_kj{
+                width: 100%;
+                height: 0.72rem;
+                .van-dropdown-menu{
+                    width: 100%;
+                    height: 0.72rem;
 
+                }
+                .van-ellipsis{
+                    height: 0.72rem;
+                    line-height: 0.72rem;
+                    font-size: 0.22rem;
+                }
+            }
+        }
+        .wll_conter{
+            width: 100%;
+            flex: 1;
+            overflow: auto;
+            background: #f1f2f5;
+            ul{
+                width: 5.86rem;
+                margin: 0 auto;
+                .wll_li{
+                    width: 5.86rem;
+                    height: 2.88rem;
+                    margin: 0 auto;
+                    margin-top: 28px;
+                    background: #ffffff;
+                    border-radius: 10px;
+                    .wll_div{
+                        width: 5.86rem;
+                        height: 2.88rem;
+                        div{
+                            width: 5.45rem;
+                            margin: 0 auto;
+                        }
+                        div:nth-of-type(1){
+                            height: 0.52rem;
+                            h2{
+                                height: 0.28rem;
+                                padding-top: 0.26rem;
+                                color: #000000;
+                            }
+                        
+                        }
+                        div:nth-of-type(2){
+                            padding-top: 0.16rem;
+                            p{
+                                color: #666666;
+                            }
+                        }
+                        div:nth-of-type(3){
+                            width: 5.45rem;
+                            height: 1.22rem;
+                            border-bottom: 1px solid #f5f5f5;
+                            ul{
+                                width: 5.45rem;
+                                height: 1.22rem;
+                                display: flex;
+                                align-items: center;
+                                li{
+                                    width: 33.33%;
+                                    height: 0.36rem;
+                                    line-height: 0.36rem;
+                                    line-height: 56px;
+                                    display: flex;
+                                    align-items: center;
+                                    img{
+                                        width: 0.36rem;
+                                        height: 0.36rem;
+                                        border-radius: 50%;
+                                    }
+                                    span{
+                                        color: #b0b0b0;
+                                        line-height: 0.36rem;
+                                        margin-left: 20px; 
+                                    }
+                                }
+                            }
+                        }
+                        div:nth-of-type(4){
+                            height: 0.74rem;
+                            display: flex;
+                            align-items: center;
+                            justify-content: space-between;
+                            p{
+                                height: 94px;
+                                line-height: 94px;
+                                color: #8c8c8c;
+                            }
+                            p:nth-of-type(2){
+                                color: #43a425;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    .van-dropdown-item__option--active, .van-dropdown-item__option--active .van-dropdown-item__icon {
+        color: red;
+    }
+    .van-dropdown-menu__title--active {
+        color: red;
+    }
 </style>
---
+++ src/components/Course/Details.vue
@@ -0,0 +1,285 @@
+<template>
+    <div class="wll_details">
+        <!-- 详情页面 -->
+        <div class="wll_destop">
+            <p>
+                <router-link class="wll_fhindex" tag="span" to="/course">&lt;</router-link> 
+                <span>课程详情</span>
+                <img src="../../../static/course-img/share.png" alt="">
+            </p>
+        </div>
+        <div v-for="(item,key) in wll_details" :key="key" class="wll_div" v-show="id==item.id">
+            <div class="wll_detdiv">
+                <p>
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/course-img/sc.png" alt="">
+                </p>
+                <p>免费</p>
+                <p>
+                    <span>{{item.lesson}}</span>
+                    <span>{{item.count}}</span>
+                </p>
+                <p>
+                    <span>开课时间:</span>
+                    <span>{{item.open}}</span>
+                </p>
+            </div>
+            <div>
+                <h2>教学团队</h2>
+                <ul>
+                    <li>
+                        <img class="wll_img1" :src="'../../../static/course-img/'+item.img" alt="">
+                        <p>{{item.teachername}}</p>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2>课程介绍</h2>
+                <p>{{item.introduce}}</p>
+            </div>
+            <div>
+                <h2>课程大纲</h2>
+                <ul>
+                    <li>
+                        <div class="wll_coldiv">
+                             <p>
+                                <span>{{item.visit}}</span>
+                                <span>{{item.outline}}</span>
+                            </p>
+                            <p>
+                                {{item.outline2}}
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <img src="../../../static/course-img/pic2.png" alt="">
+            </div>
+        </div>
+        <div class="wll_desfood">
+            <h2>立即报名</h2>
+        </div>
+    </div>
+</template>
+<script>
+    //引入axios请求器
+    import axios  from "axios";
+    export default{
+        name:"Details",
+        data() {
+            return {
+                wll_details:[],
+                id:"",
+                collect:JSON.parse(localStorage.getItem("collect")) || []
+            }
+        },
+        mounted() {
+            this.id=this.$route.query.id;
+            // this.id= item.id
+            // console.log(id);
+            axios.get("../../../static/date.json").then((res)=>{
+                console.log(res);
+                this.wll_details = res.data.data;
+            })
+        },
+        methods: {
+            // // 点击取消跳转到index页面
+            // wll_collect(item){
+            //     alert("收藏成功");
+            //     var obj = JSON.parse(localStorage.getItem("collect"))||[];
+            //     obj.push(item);
+            //     localStorage.setItem("collect",JSON.stringify(obj))
+            // },
+            // wll_qu(){
+            //     this.$router.push({
+            //         name:"serach",
+            //     })
+            // },
+            // wll_add(){
+            //     this.$router.push("/serach")
+            // }
+        },
+    }
+</script>
+<style lang="scss" scoped>
+    body,html,.wll_details{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction:column;
+        .wll_details{
+            width: 100%;
+            background: #f1f2f5;
+            .wll_destop{
+                width: 100%;
+                height: 0.74rem;
+                background: #ffffff;
+                border-bottom: 1px solid #f1f2f5;
+                p{
+                    width: 5.86rem;
+                    height: 0.74rem;
+                    margin: 0 auto;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    span{
+                        font-size: 0.26rem;
+                    }
+                    img{
+                        width: 0.3rem;
+                        height: 0.3rem;
+                    }
+                }
+            }
+            .wll_div{
+                width: 100%;
+                flex: 1;
+                overflow: auto;
+                .wll_detdiv{
+                    width: 100%;
+                    height: 2.43rem;
+                    background: #ffffff;
+                    p:nth-of-type(1){
+                        width: 5.86rem;
+                        height: 0.54rem;
+                        margin: 0 auto;
+                        padding-top: 0.36rem;
+                        display: flex;
+                        justify-content: space-between;
+                        span{
+                             font-size: 0.26rem;
+                        }
+                        img{
+                            width: 0.34rem;
+                            height: 0.34rem;
+                        }
+                    }
+                    p:nth-of-type(2){
+                        width: 5.86rem;
+                        height: 0.4rem;
+                        font-size: 0.26rem;
+                        margin: 0 auto;
+                        // padding-top: 0.16rem;
+                        color: #ee732e;
+                        display: flex;
+                        justify-content: space-between;
+                    }
+                    p:nth-of-type(3){
+                        width: 5.86rem;
+                        height: 0.4rem;
+                        font-size: 0.24rem;
+                        color: #8a857f;
+                        margin: 0 auto;
+                        span:nth-of-type(1){
+                            display: inline-block;
+                            width: 1rem;
+                            height: 0.2rem;
+                            border-right: 2px solid #98989a;
+                        }
+                    }
+                    p:nth-of-type(4){
+                        width: 5.86rem;
+                        margin: 0 auto;
+                        font-size: 0.24rem;
+                        color: #8a857f;
+                    
+                    }
+                }
+                div:nth-of-type(2){
+                    width: 100%;
+                    height: 2.42rem;
+                    background: #ffffff;
+                    margin-top: 0.26rem;
+                    h2{
+                        width: 5.86rem;
+                        display: flex;
+                        margin: 0 auto;
+                        padding-top: 0.2rem;
+                        font-size: 0.26rem;
+                    }
+                    ul{
+                        width: 5.86rem;
+                        height: 1.88rem;
+                        margin: 0 auto;
+                        display: flex;
+                        align-items: center;
+                        li{
+                            width: 1.1rem;
+                            height: 0.98rem;
+                            line-height: 0.36rem;
+                           
+                            img{
+                                width: 0.36rem;
+                                height: 0.36rem;
+                                margin: 0 auto;
+                                display: block;
+                                border-radius: 50%;
+                            }
+                            p{
+                               color: #b0b0b0;
+                                line-height: 0.36rem;
+                                margin-left: 20px; 
+                                text-align: center;
+                            }
+                        }
+                    }
+                }
+                div:nth-of-type(3){
+                    width: 100%;
+                    height: 1.08rem;
+                    margin-top: 0.26rem;
+                    background: #ffffff;
+                    h2{
+                        width: 5.86rem;
+                        margin: 0 auto;
+                        padding-top: 0.26rem;
+                    }
+                    p{
+                        width: 5.86rem;
+                        margin: 0 auto;
+                        padding-top: 0.26rem;
+                    }
+                }
+                div:nth-of-type(4){
+                    width: 100%;
+                    margin-top: 0.30rem;
+                    background: #ffffff;
+                    padding: 0.26rem;
+                    h2{
+                        width: 5.80rem;
+                        
+                    }
+                    ul{
+                        width: 5.80rem;
+                    }
+                }
+                div:nth-of-type(5){
+                    width: 100%;
+                    height: 3.46rem;
+                    background: #ffffff;
+                    img{
+                        width: 2.56rem;
+                        height: 2.76rem;
+                        margin: 0 auto;
+                        display: block;
+                    }
+                }
+            }
+            .wll_desfood{
+                width: 100%;
+                height: 86px;
+                margin-top: 20px;
+                h2{
+                    width: 100%;
+                    height: 86px;
+                    margin-top: 20px;
+                    background: #ee732e;
+                    color: #ffffff;
+                    text-align: center;
+                    line-height: 86px;
+                }
+            }
+        }
+    }
+</style>
